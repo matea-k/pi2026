b06d9d04552d3b386cf8584563181e636d868d20
cotne-AI <cotneshubitidze93@gmail.com>
2026-07-19 15:32:31 +0400

    devices.js

--- generatenumber.js
+++ generatenumber.js
@@ -9,7 +9,7 @@ fetch('pi2026-40785-default-rtdb-export.json')
     document.getElementById("humidity").textContent = data.dht11.humidity;
     document.getElementById("temperature").textContent = data.dht11.temperature
     document.getElementById("distance").textContent = data["hc-sr04"].distance
-    document.getElementById("object").textContent = data["hc-sr04"].object
-    document.getElementById("alcohol").textContent = data["hc-sr04"].alcohol
+    document.getElementById("object").textContent = data["hc-sr04"].itemDetected
+    document.getElementById("alcohol").textContent = data["mq3"].alcohol
   })
   .catch(error => console.error("there is error", error));
