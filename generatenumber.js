7e0028c783de78627d71d449531f0f9c7e9a1890
cotne-AI <cotneshubitidze93@gmail.com>
2026-07-19 17:45:52 +0400

    devices.js

--- generatenumber.js
+++ generatenumber.js
@@ -2,15 +2,19 @@
 function generateNumber() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
     document.getElementById("result").innerHTML = randomNumber;
-}   
+    
+    get();
+}
+
 function get() {
-fetch('pi2026-40785-default-rtdb-export.json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("humidity").textContent = data.dht11.humidity;
-    document.getElementById("temperature").textContent = data.dht11.temperature
-    document.getElementById("distance").textContent = data["hc-sr04"].distance
-    document.getElementById("itemDetected").textContent = data["hc-sr04"].itemDetected
-    document.getElementById("alcohol").textContent = data["mq3"].alcohol
-  })
-  .catch(error => console.error("there is error", error))}
+    fetch('pi2026-40785-default-rtdb-export.json')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("humidity").textContent = data.dht11.humidity;
+            document.getElementById("temperature").textContent = data.dht11.temperature;
+            document.getElementById("distance").textContent = data["hc-sr04"].distance;
+            document.getElementById("object").textContent = data["hc-sr04"].itemDetected;
+            document.getElementById("alcohol").textContent = data["mq3"].alcoholDetected;
+        })
+        .catch(error => console.error("there is error", error));
+} 
