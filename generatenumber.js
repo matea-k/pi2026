f943138b9a5c4d2c85cb732b75a8c95b506cc621
cotne-AI <cotneshubitidze93@gmail.com>
2026-07-19 15:20:04 +0400

    sensor.js

--- generatenumber.js
+++ generatenumber.js
@@ -3,3 +3,12 @@ function generateNumber() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
     document.getElementById("result").innerHTML = randomNumber;
 }
+fetch('pi2026-40785-default-rtdb-export.json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("humidity").textContent = data.dht11.humidity;
+    document.getElementById("temperature").textContent = data.dht11.temperature
+    document.getElementById("distance").textContent = data["hc-sr04"].distance
+
+  })
+  .catch(error => console.error("there is error", error));
