9c0fd9269a2404d38d63df4f2d01cd52c22fbe16
cotne-AI <cotneshubitidze93@gmail.com>
2026-07-19 15:56:16 +0400

    devices.js

--- generatenumber.js
+++ generatenumber.js
@@ -2,7 +2,8 @@
 function generateNumber() {
     let randomNumber = Math.floor(Math.random() * 100) + 1;
     document.getElementById("result").innerHTML = randomNumber;
-}
+}   
+function get() {
 fetch('pi2026-40785-default-rtdb-export.json')
   .then(response => response.json())
   .then(data => {
@@ -12,4 +13,4 @@ fetch('pi2026-40785-default-rtdb-export.json')
     document.getElementById("itemDetected").textContent = data["hc-sr04"].itemDetected
     document.getElementById("alcohol").textContent = data["mq3"].alcohol
   })
-  .catch(error => console.error("there is error", error));
+  .catch(error => console.error("there is error", error))}
