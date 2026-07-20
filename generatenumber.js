80b7522d31d6e73ca9815486b2248cc71ca03c10
cotne-AI <cotneshubitidze93@gmail.com>
2026-07-20 13:33:02 +0400

    gg

--- generatenumber.js
+++ generatenumber.js
@@ -4,7 +4,7 @@ function generateNumber() {
 }
 
 function get() {
-    fetch('simple.js')
+    fetch('simple.json')
         .then(response => response.json())
         .then(data => {
             document.getElementById("humidity").textContent = data.dht11.humidity;
